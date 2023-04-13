87550347ef4b42268296c7bdd0bb7385cbd535a8
StivenCarvajalCampus <StivenCarvajal3211@gmail.com>
2023-04-13 09:28:22 -0500

    feat(ProjectPoo): :sparkles: Projectfull

--- FormPOO/FormPOO/main.js
+++ FormPOO/FormPOO/main.js
@@ -7,7 +7,7 @@
 class lapiz{
     #marca
     constructor({color="#F0D000",
-                dimension="19",
+                dimension="15",
                 borrador="true", 
                 material= "Madera", 
                 marca="mongol"}){
@@ -31,19 +31,52 @@ class lapiz{
   getMaterial(){
     return this.material;
   }
+  getMarca(){
+    return this.#marca;
+
+  }
 }
 let obj = undefined;
 let color = document.querySelector(`[name="color"]`)
 let dimension = document.querySelector(`[name="range"]`)
 let borrador = tiene.checked= true;
 let material = Madera.checked = true ;
+let marca = Mongo.checked = true;
 
 addEventListener("DOMContentLoaded", (e)=>{
     obj = new lapiz({});
     color.value = obj.color;
-    dimension.value = obj.dimension;
-    material.getElementById("Madera") = obj.material; 
-    borrador.getElementById("tiene") = obj.borrador;
+    dimension.value = obj.getDimension;
+    document.getElementById("Madera").innerHTML = obj.getMaterial(); 
+    document.getElementById("tiene").innerHTML = obj.getBorrador();
+    document.getElementById("Mongo").innerHTML = obj.getMarca();
 
+    const form = document.querySelector("#formulario")
+    const table = document.querySelector("#table")
+
+    formulario.addEventListener("submit",(e) => {
+      e.preventDefault();
+      const formData = new FormData(form);
+      const colorData = formData.get("color");
+      const dimensionData = formData.get("range");
+      const marcaData = formData.get("marca");
+      const borradorData = formData.get("borrador");
+      const materialData = formData.get("material");
+
+      const pencil1 = new lapiz ({color: colorData, dimension: dimensionData, marca: marcaData,borrador: borradorData, material: materialData})
+
+      let pencil = new lapiz({}) ;
+      document.querySelector(`#table`).insertAdjacentHTML("beforeend",` 
+      <tr>
+      <th style = "background-color:${pencil1.getColor()}">${pencil1.getColor()}</th>
+      <th>${pencil1.getDimension()}</th>
+      <th>${pencil1.getMarca()}</th>
+      <th>${pencil1.getBorrador()}</th>
+      <th>${pencil1.getMaterial()}</th>
+    </tr>
+    `)
+    
+    
+            })
 
 })
